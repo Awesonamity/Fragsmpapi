aeee7b65adfd5ade96f3955adfbad9de1b15d4e0
Cabbage838 <85857757+Cabbage698@users.noreply.github.com>
2025-07-03 14:00:51 -0600

    Add files via upload

---
+++ client/java/dev/awesonamity/fragsmpapi/client/FragsmpapiClient.java
@@ -0,0 +1,10 @@
+package dev.awesonamity.fragsmpapi.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FragsmpapiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ java/dev/awesonamity/fragsmpapi/Fragsmpapi.java
@@ -0,0 +1,10 @@
+package dev.awesonamity.fragsmpapi;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fragsmpapi implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
